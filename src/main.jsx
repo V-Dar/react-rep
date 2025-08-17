5c1f9c9a9c62b142050882121d47f47b1905402f
Victor Dar <victordar@mail.ru>
2025-08-17 11:22:24 +0300

    создан реакт-проект

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+
+createRoot(document.getElementById('root')).render(
+  <div>
+    <h1>Hello world!</h1>
+    <p>Какой-то текст</p>
+    <p>Какой-то текст</p>
+    <p>Какой-то текст</p>
+    <p>Какой-то текст</p>
+  </div>
+)
